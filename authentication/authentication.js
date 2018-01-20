ca474403e53d9629b6ee1ef9547e288bc6461ad2
Henry <hmd1987@gmail.com>
2018-01-20 11:34:51 -0600

    install bcrypt package & add prehook to mongoose schema

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -55,4 +55,20 @@ if (req.body.email &&
     }
   });
 }
+/**
+"install the bcrypt package & add prehook to mongoose schema"
+**/
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+/**
 
+**/
