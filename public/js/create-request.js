e685afff401d1ae496ce981a2a888dbd949b98aa
Jason Saenz <jsigns@gmail.com>
2018-01-20 14:02:30 -0600

    ugh

---
+++ public/js/create-request.js
@@ -0,0 +1,58 @@
+var requestInfo;
+$(document).ready(function() {
+  $("#saveBtn").on("click", function(e) {
+    e.preventDefault();
+    var modalInfo = {
+      itemName: $("#itemName")
+        .val()
+        .trim(),
+      quantity: $("#quant")
+        .val()
+        .trim(),
+      measureUnit: $("#measureUnit").val(),
+      estimatedPrice: $("#estPrice")
+        .val()
+        .trim()
+    };
+    console.log("farts");
+    $("#newItemName").append(`<p>Item Name: ${modalInfo.itemName}</p>`);
+    $("#newItemQuant").append(`<p>Quantity: ${modalInfo.quantity}</p>`);
+    $("#newItemMeasure").append(
+      `<p>Unit of Measure: ${modalInfo.measureUnit}</p>`
+    );
+    $("#newItemPrice").append(
+      `<p>Estimated Price: $${modalInfo.estimatedPrice}</p>`
+    );
+    console.log("poo");
+  });
+  $("#saveRequest").on("click", function() {
+    requestInfo = {
+      dateNeeded: $("#dateNeeded")
+        .val()
+        .trim(),
+      itemName: $("#itemName")
+        .val()
+        .trim(),
+      quantity: $("#quant")
+        .val()
+        .trim(),
+      measureUnit: $("#measureUnit").val(),
+      estimatedPrice: $("#estPrice")
+        .val()
+        .trim(),
+      taxRate: $("#taxRate")
+        .val()
+        .trim(),
+      estimatedShipping: $("#estShip")
+        .val()
+        .trim(),
+      justification: $("#justify")
+        .val()
+        .trim(),
+      comments: $("#comments")
+        .val()
+        .trim()
+    };
+  });
+});
+module.exports = requestInfo;
