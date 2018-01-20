67fecd97ffaf5efd749a27c079a7f42e767881ac
Henry <hmd1987@gmail.com>
2018-01-20 13:29:01 -0600

    middleware added

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -101,3 +101,42 @@ UserSchema.statics.authenticate = function (email, password, callback) {
       })
     });
 }
+/*
+"create middleware to make user IDs available in HTML,
+create logout  route that destroys the session id & redireccts back to the home route.
+*/
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    var err = new Error('You must be logged in to view this page.');
+    err.status = 401;
+    return next(err);
+  }
+}
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
+  //...
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
