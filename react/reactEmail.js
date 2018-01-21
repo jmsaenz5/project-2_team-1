6241680473ae2bc7ea84c00da14efdd92983db51
arodg <anitarodgerson@gmail.com>
2018-01-21 14:03:39 -0600

    Track user changes

--- react/reactEmail.js
+++ react/reactEmail.js
@@ -1,3 +1,5 @@
+// React code to send emails upon submission of Create Request form
+
 const purchaseRequestForm = document.querySelector('create-request.js');
 
 class ReactFormLabel extends React.Component {
@@ -24,3 +26,37 @@ class ReactForm extends React.Component {
  	}
 }
 
+// Keeps track of any user changes
+userChange = (e) => {
+ let newState = {};
+
+ newState[e.target.name] = e.target.value;
+
+ this.setState(newState);
+};
+
+
+// Stores user changes
+let userData = {
+  formSender: this.state.name,
+  formEmail: this.state.email,
+  formSubject: this.state.subject,
+  formMessage: this.state.message
+}
+$.ajax({
+  url: '/create-request.js',
+  dataType: 'json',
+  type: 'POST',
+  data: userData,
+  success: function(data) {
+  	if (confirm('Yes')) {
+    	document.querySelector('.form-input').val('');
+   	}
+  },
+  error: function(xhr, status, err) {
+  	console.error(status, err.toString());
+   	alert('There was some problem with sending your message.');
+  }
+ });
+
+ 
