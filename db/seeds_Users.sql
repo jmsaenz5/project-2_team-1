b2ff2dd01b740b787e1b5724cdf24ec526fc351d
arodg <anitarodgerson@gmail.com>
2018-01-25 14:57:17 -0600

    Add seeds file for Users

---
+++ db/seeds_Users.sql
@@ -0,0 +1,140 @@
+-- (1 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'Browne', 
+	'Sade',
+	'sbribrowne@gmail.com', 
+	12345,
+	'initiator',
+	'BrowneS'
+);
+
+-- (2 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'Saenz', 
+	'Jason',
+	'saenzj@trax.com', 
+	54321,
+	'admin',
+	'SaenzJ'
+);
+
+-- (3 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'Rodgerson', 
+	'Anita',
+	'aadk@runbox.com', 
+	34521,
+	'initiator',
+	'RodgersonA'
+);
+
+-- (4 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'Usrey-Scott', 
+	'Jenny',
+	'sylvrleef@gmail.com', 
+	33451,
+	'admin',
+	'Usrey-ScottJ'
+);
+
+-- (5 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'Dillard', 
+	'Henry',
+	'hmd1987@gmail.com', 
+	22345,
+	'reviewer',
+	'DillardH'
+);
+
+-- (6 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES 
+(
+	'White', 
+	'Jack',
+	'whitej@trax.com', 
+	23451,
+	'reviewer',
+	'WhiteJ'
+);
+
+-- (7 of 7) Dummy data Users
+INSERT INTO Users
+(
+	Last_Name, 
+	First_Name,
+	Email, 
+	Staff_ID, 
+	Security_Role,
+	Login_Name 
+)
+VALUES (
+	'Green', 
+	'Amanda',
+	'greena@trax.com', 
+	54123,
+	'reviewer',
+	'GreenA'
+);
+    
+    
