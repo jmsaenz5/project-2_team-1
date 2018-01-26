d1dd74eeb8b4d88bd811f6d0584cd3fe2159710f
Sylvrleef <sylvrleef@gmail.com>
2018-01-25 19:02:24 -0600

    Removed unused file.

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-// const Sequelize = require("sequelize");
-//
-// const sequelize = new Sequelize("project2", "root", "^yours001", {
-//   host: "localhost",
-//   dialect: "mysql"
-// });
-//
-// module.exports = sequelize;
