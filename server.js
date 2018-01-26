4c98d70ed0d0e48a4f4bdfd212bdda34a107d7db
Sylvrleef <sylvrleef@gmail.com>
2018-01-26 10:13:24 -0600

    Removed old server.js and renamed app.js to server.js

--- server.js
+++ server.js
@@ -1,37 +1,72 @@
-// // Dependencies
-// const express = require("express");
-// const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const app = express();
+const db = require("./models");
 
-// // Sets up the Express App
-// const app = express();
-// const PORT = process.env.PORT || 3000;
 
-// // Requiring our models for syncing
-// const db = require("./models");
+// use sessions for tracking logins
+app.use(session({
+  secret: 'trax rox',
+  resave: true,
+  saveUninitialized: false
+}));
 
-// // Sets up the Express app to handle data parsing
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// make user ID available in templates
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.session.userId;
+  next();
+});
 
-// // Static directory
-// app.use(express.static("public"));
+// mongodb connection
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+mongoose.connect("mongodb://localhost:27017");
+}
 
-// // Set handlebars.
-// const exphbs = require("express-handlebars");
+var mdb = mongoose.connection;
+// mongo error
+mdb.on('error', console.error.bind(console, 'connection error:'));
 
-// app.engine("handlebars", exphbs({defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// // Import routes and give the server access to them.
-// const routes = require("./controllers/controller.js")
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
 
-// app.use("/", routes);
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// include routes
+var routes = require('./routes/index');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 // // Syncing our sequelize models and then starting our Express app
-// db.sequelize.sync({}).then(function() {
-//   app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-// });
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
