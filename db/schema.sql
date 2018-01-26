c8925c6c59a4fc5bb73694f12b5cbe06b5e52de4
Henry <hmd1987@gmail.com>
2018-01-25 20:52:59 -0600

    minor adjustments to the schema once again. getting it almost to exactly how we want it

--- db/schema.sql
+++ db/schema.sql
@@ -7,31 +7,36 @@ CREATE TABLE Purchase_Request
 (
     PR_Number int AUTO_INCREMENT NOT NULL,
     Date_Created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    Item_Description varchar(400) NOT NULL,
-    Date_Needed date NOT NULL,
-    Qty int NOT NULL,
-    Unit varchar(50) NOT NULL,
-    Vendor varchar(100) NOT NULL,
-    Est_Price DECIMAL(10,2) NOT NULL,
+    Date_Needed DATE NOT NULL,
     Tax_Rate DECIMAL(5,2) NOT NULL,
     Est_Shipping DECIMAL (10,2) NOT NULL,
-    Justification varchar(500) NOT NULL,
-    Comments varchar(500),
-    PR_Status varchar(50),
-    Date_Completed date,
-    Created_By varchar(100),
+    Justification VARCHAR(500) NOT NULL,
+    Comments VARCHAR(500),
+    PR_Status VARCHAR(50),
+    Date_Completed DATE,
+    Created_By VARCHAR(100),
     PRIMARY KEY (PR_Number)
 );
 
 CREATE TABLE Users
 (
     ID int AUTO_INCREMENT NOT NULL,
-    Last_Name varchar(100) NOT NULL,
-    First_Name varchar(100) NOT NULL,
-    Email varchar(200) NOT NULL,
-    Staff_ID integer NOT NULL,
-    Security_Role varchar(100) NOT NULL,
-    Login_Name varchar(100) NOT NULL,
+    Last_Name VARCHAR(100) NOT NULL,
+    First_Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(200) NOT NULL,
+    Staff_ID INTEGER NOT NULL,
+    Security_Role VARCHAR(100) NOT NULL,
+    Login_Name VARCHAR(100) NOT NULL,
     PRIMARY KEY (ID)
+);
+
+CREATE TABLE Items
+(
+    Qty INTEGER(100) NOT NULL,
+    Unit VARCHAR(100) NOT NULL,
+    Vendor VARCHAR(100) NOT NULL,
+    Est_Price DECIMAL(10,2) NOT NULL,
+    PR_Number INTEGER NOT NULL,
+    Item_Description VARCHAR(100) NOT NULL
 );
 
