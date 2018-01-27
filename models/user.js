942ce2cf2c0cdf00038749be5ecc8fe52b6c1d0a
arodg <anitarodgerson@gmail.com>
2018-01-26 23:32:17 -0600

    Add active status column (boolean) to Users table

--- models/user.js
+++ models/user.js
@@ -1,20 +1,15 @@
 const Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define("User", {
+  const User = sequelize.define("Users", {
     Last_Name: DataTypes.STRING,
     First_Name: DataTypes.STRING,
     Email: DataTypes.STRING,
     Staff_ID: DataTypes.INTEGER,
     Security_Role: DataTypes.STRING,
-    Login_Name: DataTypes.STRING
+    Login_Name: DataTypes.STRING,
+    Active_Status: { DataTypes.BOOLEAN, allowNull: false, defaultValue: true }
   });
 
-  // Initiator.associate = function(models) {
-  //   Initiator.hasMany(models.purchaseRequest, {});
-  // };
-
   return User;
-
-  // Initiator.sync();
 };
